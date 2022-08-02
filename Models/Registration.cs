6de1bf2f34797503211c57ec6a47b3b4e4bf7903
Saurabh Nehe <Saurabh.Nehe@data-axle.com>
2022-08-02 17:10:19 +0530

    first

---
+++ Models/Registration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoginSystem.Models
+{
+    public class Registration
+    {
+        [Key]
+        public long Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public string Email { get; set; }
+
+    }
+}
